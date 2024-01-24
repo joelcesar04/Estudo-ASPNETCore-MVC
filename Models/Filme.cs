701e97c5653fbbba02dbc337223e8a447c402319
Joel Andrade <joelcesar09@gmail.com>
2024-01-24 07:06:37 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Filme.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DemoMVC.Models
+{
+    public class Filme
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required(ErrorMessage = "O campo Título é obrigatório.")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "O Título precisa ter de 3 à 60 caracteres.")]
+        public string Titulo { get; set; }
+
+        [DataType(DataType.DateTime, ErrorMessage = "Data em formato incorreto.")]
+        [Required(ErrorMessage = "O campo Data de Lancaçamento é obrigatório.")]
+        [Display(Name = "Data de Lançamento.")]
+        public DateTime DataLancamento { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\u00C0-\u00FF""'\w-]*$", ErrorMessage = "Gênero em formato inválido.")]
+        [StringLength(30, ErrorMessage = "Máximo 30 caracteres."), Required(ErrorMessage = "O campo gênero é obrigatório.")]
+        public string Genero { get; set; }
+
+        [Range(1, 1000, ErrorMessage = "Valor de 1 a 1000")]
+        [Required(ErrorMessage = "Preencha o campo Valor")]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Valor { get; set; }
+
+        [Required(ErrorMessage = "Preencha o campo Avaliação")]
+        [Display(Name = "Avaliação")]
+        [RegularExpression(@"^[0-5]*$", ErrorMessage = "Somente números")]
+        public int Avaliacao { get; set; }
+    }
+}
