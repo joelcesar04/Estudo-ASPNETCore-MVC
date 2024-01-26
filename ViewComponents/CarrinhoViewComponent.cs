d251ab8d89e5a5e7915938d31a0f43c141687592
Joel Andrade <joelcesar09@gmail.com>
2024-01-26 13:43:20 -0300

    Criação Component

---
+++ ViewComponents/CarrinhoViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoMVC.ViewComponents
+{
+    [ViewComponent(Name = "Carrinho")]
+    public class CarrinhoViewComponent : ViewComponent
+    {
+        public CarrinhoViewComponent()
+        {
+            ItensCarrinho = 3;
+        }
+        public int ItensCarrinho { get; set; }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(ItensCarrinho);
+        }
+    }
+}
